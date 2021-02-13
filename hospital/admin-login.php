716f4114517eb9a88a188c0ea7ba1164dc1aa32a
vedansh-srivastava <72248253+vedansh-srivastava@users.noreply.github.com>
2021-02-13 23:37:54 +0530

    Add files via upload
    I have made only the welcome screen for the app. Check it out.

---
+++ hospital/admin-login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Login</title>
+  </head>
+  <body>
+    <h1>Admin will login using this only.</h1>
+  </body>
+</html>
---
+++ hospital/patient-signup.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sign Up</title>
+  </head>
+  <body>
+    <h1>This is sign up for patients.</h1>
+  </body>
+</html>
